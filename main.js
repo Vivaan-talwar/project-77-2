ca01e319def75d8d41f5e46b8575befec452385c
Vivaan-talwar <87641773+Vivaan-talwar@users.noreply.github.com>
2021-07-19 14:24:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var name_1=vivaan;
+console.log(name_1);
+var name_2=talwar;
+console.log(name_2);
+
+var full_name=name_1.concat(name_2);
+
+var num_1_add=20;
+var num_2_add=30;
+
+var add=num_1_add+num_2_add;
+console.log(add);
+
+var num_1_sub;
+var num_2_sub;
+
+var sub=num_1_sub-num_2_sub;
+console.log(sub);
